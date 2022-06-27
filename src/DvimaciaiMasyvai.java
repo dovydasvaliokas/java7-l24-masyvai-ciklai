229a58964b2ff5e0c14e6f6f1ed0342cadb376d7
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-06-27 15:20:46 +0300

    dvimacio nelabai veikiantis pavyzdys

--- src/DvimaciaiMasyvai.java
+++ src/DvimaciaiMasyvai.java
@@ -1,5 +1,35 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * NEBAIGTAS IR NELABAI VEIKIANTIS PAVYZDYS
+ */
 public class DvimaciaiMasyvai {
     public static void main(String[] args) {
-
+        char[][] namoPlanas = dvimacioMasyvoNuskaitymas();
+        System.out.println("namoPlanas = " + Arrays.deepToString(namoPlanas));
     }
+
+
+
+    static public char[][] dvimacioMasyvoNuskaitymas() {
+        Scanner skaitytuvas = new Scanner(System.in);
+        System.out.println("Įveskite ilgį: ");
+        int eilutes = skaitytuvas.nextInt();
+        System.out.println("Įveskite plotį: ");
+        int stulpeliai = skaitytuvas.nextInt();
+
+        char[][] masyvas = new char[eilutes][stulpeliai];
+
+        for (int i = 0; i < eilutes; i++) {
+            for (int j = 0; j < stulpeliai; j++) {
+                masyvas[i][j] = skaitytuvas.next().charAt(0);
+            }
+
+        }
+
+        return masyvas;
+    }
+
+
 }
